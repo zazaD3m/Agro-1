d3935dac941440fe4cc18a68e4c8a632f0a49f67
zazaD3m <zaza.d3m@gmail.com>
2024-04-13 14:49:33 +0400

    ready to deploy

--- server/src/app.js
+++ server/src/app.js
@@ -5,7 +5,8 @@ import cookieParser from "cookie-parser";
 import morgan from "morgan";
 import rootRoutes from "./routes/index.js";
 import globalErrorHandler from "./controllers/errorController.js";
-import { CLIENT_URL, isProduction } from "./config/config.js";
+import { CLIENT_URL, isDevelopment, isProduction } from "./config/config.js";
+import { CustomError } from "./utils/CustomError.js";
 
 const app = express();
 
@@ -20,22 +21,34 @@ app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 app.use(express.static("public"));
 app.use(cookieParser());
-app.use(morgan("dev"));
+if (isDevelopment) {
+  app.use(morgan("dev"));
+}
+
+const __dirname = path.resolve();
+
+console.log(__dirname);
 
 app.use("/api", rootRoutes);
 
 if (isProduction) {
   const __dirname = path.resolve();
-  // making dist static folder
-  app.use(express.static(path.join(__dirname, "frontend/dist")));
+  const distPath = path.join(__dirname, "../client/dist");
 
-  // any route thats not /api/users gonna load fronend index.html
-  app.get("*", (req, res) =>
-    res.sendFile(path.resolve(__dirname, "frontend", "dist", "index.html"))
-  );
+  // Serve static files from the 'dist' folder
+  app.use(express.static(distPath));
+
+  // For any route not handled by the server, serve the 'index.html' file
+  app.get("*", (req, res) => {
+    res.sendFile(path.join(distPath, "index.html"));
+  });
 } else {
   app.get("/", (req, res) => res.send("Server is ready"));
 }
+
+app.all("*", (req, res, next) => {
+  throw new CustomError(`Can't find ${req.originalUrl} on the server!`, 404);
+});
 
 app.use(globalErrorHandler);
 
