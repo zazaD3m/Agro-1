48dc1deb375181ede5b47a319ef22144bf22f50b
zazaD3m <zaza.d3m@gmail.com>
2024-05-27 00:52:58 +0400

    aasdfa

--- client/src/components/navigation/Footer.jsx
+++ client/src/components/navigation/Footer.jsx
@@ -2,15 +2,21 @@ import useWindowSize from "@/hooks/useWindowSize";
 import { MAIN_CATEGORIES } from "./categories-data";
 import { Link } from "react-router-dom";
 import { Facebook, Instagram } from "lucide-react";
+import {
+  Accordion,
+  AccordionContent,
+  AccordionItem,
+  AccordionTrigger,
+} from "../ui/accordion";
 
 const Footer = () => {
   const { width } = useWindowSize();
 
   return (
-    <footer className="bg-background-green pb-16 pt-8 lg:pb-0">
+    <footer className="bg-background-green pb-20 pt-8 lg:pb-0">
       <div className="container px-2 lg:px-4">
         {width >= 1024 && (
-          <div className="flex flex-wrap gap-y-16 pb-6">
+          <div className="flex flex-wrap gap-y-16">
             <div className="basis-1/4">
               <h2 className="mb-6 font-medium">დახმარება</h2>
               <ul className="flex flex-col gap-y-4 text-xs">
@@ -52,24 +58,6 @@ const Footer = () => {
                   </a>
                 </li>
               </ul>
-              <div className="flex gap-x-4 pt-6">
-                <a
-                  target="_blank"
-                  rel="noreferrer"
-                  className="flex size-10 items-center justify-center rounded-full bg-gray-200 hover:bg-gray-300"
-                  href="https://www.facebook.com/agroezo.ge/"
-                >
-                  <Facebook strokeWidth={0} className="size-5 fill-black" />
-                </a>
-                <a
-                  target="_blank"
-                  rel="noreferrer"
-                  className="flex size-10 items-center justify-center rounded-full bg-gray-200 hover:bg-gray-300"
-                  href="https://www.instagram.com/agroezo.ge/"
-                >
-                  <Instagram strokeWidth={2} className="size-5 text-black" />
-                </a>
-              </div>
             </div>
             <div className="basis-3/4">
               <h2 className="mb-6 font-medium">კატეგორიები</h2>
@@ -88,8 +76,93 @@ const Footer = () => {
             </div>
           </div>
         )}
-        {/* {width < 1024 && <div></div>} */}
-        <div className="flex flex-col gap-x-6 gap-y-4 border-y border-black/10 py-3 text-sm lg:flex-row">
+        <div className="flex gap-x-4 py-4 lg:py-6">
+          <a
+            target="_blank"
+            rel="noreferrer"
+            className="flex size-10 items-center justify-center rounded-full bg-gray-200 hover:bg-gray-300"
+            href="https://www.facebook.com/agroezo.ge/"
+          >
+            <Facebook strokeWidth={0} className="size-5 fill-black" />
+          </a>
+          <a
+            target="_blank"
+            rel="noreferrer"
+            className="flex size-10 items-center justify-center rounded-full bg-gray-200 hover:bg-gray-300"
+            href="https://www.instagram.com/agroezo.ge/"
+          >
+            <Instagram strokeWidth={2} className="size-5 text-black" />
+          </a>
+        </div>
+        {width < 1024 && (
+          <Accordion type="multiple">
+            <AccordionItem value="დახმარება">
+              <AccordionTrigger>დახმარება</AccordionTrigger>
+              <AccordionContent>
+                <ul className="space-y-4">
+                  <li>
+                    <Link
+                      to="/info/add-product"
+                      className="text-xs opacity-80 hover:opacity-100 sm:text-sm"
+                    >
+                      განცხადების დამატების ინსტრუქცია
+                    </Link>
+                  </li>
+                  <li>
+                    <Link
+                      to="/info/faq"
+                      className="text-xs opacity-80 hover:opacity-100 sm:text-sm"
+                    >
+                      ხშირად დასმული კითხვები
+                    </Link>
+                  </li>
+                  <li>
+                    <Link
+                      to="/info/contact"
+                      className="text-xs opacity-80 hover:opacity-100 sm:text-sm"
+                    >
+                      მესიჯის მიწერა
+                    </Link>
+                  </li>
+                  <li>
+                    <a
+                      href="tel:(032) 212 53 17"
+                      className="text-xs opacity-80 hover:opacity-100 sm:text-sm"
+                    >
+                      (032) 212 53 17
+                    </a>
+                  </li>
+                  <li>
+                    <a
+                      href="mailto:agroezo@gmail.com"
+                      className="text-xs opacity-80 hover:opacity-100 sm:text-sm"
+                    >
+                      agroezo@gmail.com
+                    </a>
+                  </li>
+                </ul>
+              </AccordionContent>
+            </AccordionItem>
+            <AccordionItem value="კატეგორიები">
+              <AccordionTrigger>კატეგორიები</AccordionTrigger>
+              <AccordionContent>
+                <ul className="space-y-6">
+                  {MAIN_CATEGORIES.map((cat) => (
+                    <li key={cat.name}>
+                      <Link
+                        to={"/catalog/" + cat.link}
+                        className="opacity-80 hover:opacity-100"
+                      >
+                        {cat.name}
+                      </Link>
+                    </li>
+                  ))}
+                </ul>
+              </AccordionContent>
+            </AccordionItem>
+          </Accordion>
+        )}
+        <div className="flex flex-col gap-x-6 gap-y-4 border-b border-black/10 py-3 text-xs sm:flex-row sm:text-sm lg:border-y">
           <Link
             to="/info/terms-of-use"
             className="underline underline-offset-2 opacity-80 hover:opacity-100"
