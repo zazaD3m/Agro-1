215655df1c796729f2e726567631ff9e7ba468fe
zazaD3m <zaza.d3m@gmail.com>
2024-10-03 23:38:11 +0400

    added hero photos

--- client/src/pages/home/components/HomePageHeroCarousel.jsx
+++ client/src/pages/home/components/HomePageHeroCarousel.jsx
@@ -8,7 +8,7 @@ import Autoplay from "embla-carousel-autoplay";
 
 const HOME_PAGE_CONTENT = [
   {
-    img: "hero-3.webp",
+    img: "test-3.png",
     heading:
       "აგროეზო - ონლაინ პლატფორმა, რომელიც აერთიანებს ადგილობრივ ფერმერს და მომხმარებელს! საქონლის გაყიდვა და შეძენა!",
     p: "ონლაინ ბაზარი, პირდაპირი კავშირი პროდუქტის განმთავსებელსა და მომხმარებელს შორის. საბითუმო და საცალო ვაჭრობა. გამოგზავნით და ადგილზე შეძენით",
@@ -28,7 +28,7 @@ const HOME_PAGE_CONTENT = [
     ],
   },
   {
-    img: "hero-1.webp",
+    img: "test-2.png",
     heading: `მყიდველებს - იყიდეთ ნებისმიერი პროდუქტი აგროსფეროდან აგროეზოში, შუამავლის, ფასნამატის და საკომისიოს გარეშე! შეუზღუდავი არჩევანი!`,
     p: "ორგანული საკვები ზაფხულის მაცხოვრებლებისა და ფერმერებისგან. მიტანით და პიკაპით. საბითუმო და საცალო ვაჭრობა.",
     buttons: [
@@ -39,7 +39,52 @@ const HOME_PAGE_CONTENT = [
     ],
   },
   {
-    img: "hero-2.webp",
+    img: "test-1.png",
+    heading: "გამყიდველებისთვის - დაამატეთ თქვენი პროდუქტები სრულიად უფასოდ!",
+    p: "არანაირი საკომისიო. ჩვენი საიტი კარგად არის ინდექსირებული საძიებო სისტემებში. მოსახერხებელი პირადი ანგარიში, საქონლის მარტივი დამატება, საქონლის შეკვეთა ვებგვერდზე.",
+    buttons: [
+      {
+        linkText: "გაყიდე",
+        link: "/account/add-new-product",
+      },
+      {
+        linkText: "ინსტრუქცია",
+        link: "/info/add-product",
+      },
+    ],
+  },
+  {
+    img: "test-4.png",
+    heading: "გამყიდველებისთვის - დაამატეთ თქვენი პროდუქტები სრულიად უფასოდ!",
+    p: "არანაირი საკომისიო. ჩვენი საიტი კარგად არის ინდექსირებული საძიებო სისტემებში. მოსახერხებელი პირადი ანგარიში, საქონლის მარტივი დამატება, საქონლის შეკვეთა ვებგვერდზე.",
+    buttons: [
+      {
+        linkText: "გაყიდე",
+        link: "/account/add-new-product",
+      },
+      {
+        linkText: "ინსტრუქცია",
+        link: "/info/add-product",
+      },
+    ],
+  },
+  {
+    img: "test-5.png",
+    heading: "გამყიდველებისთვის - დაამატეთ თქვენი პროდუქტები სრულიად უფასოდ!",
+    p: "არანაირი საკომისიო. ჩვენი საიტი კარგად არის ინდექსირებული საძიებო სისტემებში. მოსახერხებელი პირადი ანგარიში, საქონლის მარტივი დამატება, საქონლის შეკვეთა ვებგვერდზე.",
+    buttons: [
+      {
+        linkText: "გაყიდე",
+        link: "/account/add-new-product",
+      },
+      {
+        linkText: "ინსტრუქცია",
+        link: "/info/add-product",
+      },
+    ],
+  },
+  {
+    img: "test-6.png",
     heading: "გამყიდველებისთვის - დაამატეთ თქვენი პროდუქტები სრულიად უფასოდ!",
     p: "არანაირი საკომისიო. ჩვენი საიტი კარგად არის ინდექსირებული საძიებო სისტემებში. მოსახერხებელი პირადი ანგარიში, საქონლის მარტივი დამატება, საქონლის შეკვეთა ვებგვერდზე.",
     buttons: [
@@ -72,12 +117,12 @@ const HomePageHeroCarousel = ({ className }) => {
         // ]}
       >
         <CarouselContent>
-          {images.map((data, i) => (
+          {HOME_PAGE_CONTENT.map((data, i) => (
             <CarouselItem key={i}>
-              <div className="relative">
+              <div className="relative flex justify-center">
                 <img
-                  src={`bgs/${data}.jpg`}
-                  className="h-80 w-full rounded-b-md object-cover brightness-[.8] sm:h-[450px]"
+                  src={data.img}
+                  className="h-80 w-auto bg-background object-contain sm:h-[450px]"
                 />
                 {/* <div className="absolute left-0 top-0 w-full space-y-4 p-4 pt-3 sm:p-6 sm:pl-8 xl:p-8 xl:pl-12">
                   <h1 className="cursor-default text-pretty break-words font-semibold tracking-wide text-white sm:max-w-md sm:text-lg md:max-w-lg lg:text-xl xl:max-w-2xl xl:text-2xl">
