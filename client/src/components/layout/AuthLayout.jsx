721d094c39ca5761cb9be46b11b5b4267f5fa495
zazaD3m <zaza.d3m@gmail.com>
2024-10-09 15:47:37 +0400

    asdfhadfsj

--- client/src/components/layout/AuthLayout.jsx
+++ client/src/components/layout/AuthLayout.jsx
@@ -2,20 +2,20 @@ import { Link, Outlet } from "react-router-dom";
 
 const AuthLayout = () => {
   return (
-    <main className="w-full sm:grid sm:grid-cols-4">
-      <section className="mx-auto mt-12 flex max-w-md flex-col px-4 sm:col-span-3 sm:min-w-[26rem] lg:col-span-2">
-        <h1 className="mb-12 text-4xl text-primary">
-          <Link to="/">ლოგო</Link>
-        </h1>
-        <div className="space-y-6">
+    <main className="w-full lg:grid lg:grid-cols-2">
+      <div className="h-screen overflow-y-auto">
+        <section className="mx-auto flex max-w-md flex-col px-4 sm:min-w-[26rem] lg:col-span-1">
+          <Link to="/" className="py-12 text-4xl text-primary">
+            ლოგო
+          </Link>
           <Outlet />
-        </div>
-      </section>
-      <section className="hidden h-screen bg-muted sm:col-span-1 sm:block lg:col-span-2">
+        </section>
+      </div>
+      <section className="hidden h-screen overflow-hidden bg-muted lg:col-span-1 lg:block">
         <img
-          src="/placeholder.svg"
+          src="/test-5.png"
           alt="Image"
-          className="h-full w-full object-cover"
+          className="h-screen w-full object-cover object-left"
         />
       </section>
     </main>
